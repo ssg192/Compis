88a304b85a2c8d1ffb8ac8e3f92924bd61f9bfa9
Salvador Sandoval Garibay <emeth.salvador@ceneval.edu.mx>
2025-02-20 12:36:12 -0600

    feat(repl) update de funciones para el repl

--- repl/src/main/java/org/example/Main.java
+++ repl/src/main/java/org/example/Main.java
@@ -5,43 +5,47 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args)throws FileNotFoundException{
+    public static void main(String[] args) throws FileNotFoundException {
 
-        if(args.length > 1){
+        if (args.length > 1) {
             System.out.println("Se recibio mas de un argumento, solo se permite 0 a 1 argumento");
             System.exit(1);
         }
 
-        Scanner entradaTerminal = new Scanner(System.in);
         if (args.length == 0) {
-            while (true) {
-                System.out.print("REPL-ESCOM: ");
-                if (!entradaTerminal.hasNextLine()) {
-                    break;
-                }
-                String entrada = entradaTerminal.nextLine();
-                if (entrada.equalsIgnoreCase("/exit")) {
-                    System.out.println("Goodbye");
-                    break;
-                }
-                System.out.println(entrada);
-            }
-            entradaTerminal.close();
-
+            ejecutarRepl();
         } else {
-            File rutaDelArchivo = new File("src/repl.txt");
-            if (!rutaDelArchivo.exists() || !rutaDelArchivo.isFile()) {
-                System.out.println("Error: El archivo especificado no existe o no es un archivo válido.");
-                System.exit(1);
-            }
-            System.out.println(rutaDelArchivo);
-            Scanner lecturaArchivo = new Scanner(rutaDelArchivo);
-            while (lecturaArchivo.hasNextLine()) {
-                System.out.println(lecturaArchivo.nextLine());
-            }
+            leerArchivo("src/repl.txt");
         }
-
-
     }
 
+    public static void ejecutarRepl() {
+        Scanner entradaTerminal = new Scanner(System.in);
+        while (true) {
+            System.out.print("REPL-ESCOM: ");
+            if (!entradaTerminal.hasNextLine()) {
+                break;
+            }
+            String entrada = entradaTerminal.nextLine();
+            if (entrada.equalsIgnoreCase("/exit")) {
+                System.out.println("Goodbye");
+                break;
+            }
+            System.out.println(entrada);
+        }
+        entradaTerminal.close();
     }
+
+    public static void leerArchivo(String ruta) throws FileNotFoundException {
+        File rutaDelArchivo = new File(ruta);
+        if (!rutaDelArchivo.exists() || !rutaDelArchivo.isFile()) {
+            System.out.println("Error: El archivo especificado no existe o no es un archivo válido.");
+            System.exit(1);
+        }
+        System.out.println(rutaDelArchivo);
+        Scanner lecturaArchivo = new Scanner(rutaDelArchivo);
+        while (lecturaArchivo.hasNextLine()) {
+            System.out.println(lecturaArchivo.nextLine());
+        }
+    }
+}
