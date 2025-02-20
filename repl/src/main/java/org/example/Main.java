1ff76c6329c5b3a194cb3bb1df7a4b07c4b7a506
Salvador Sandoval Garibay <emeth.salvador@ceneval.edu.mx>
2025-02-20 11:45:24 -0600

    feat(repl) update de file y entrada scanner

--- repl/src/main/java/org/example/Main.java
+++ repl/src/main/java/org/example/Main.java
@@ -1,24 +1,45 @@
 package org.example;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws FileNotFoundException{
+
+        if(args.length > 1){
+            System.out.println("Se recibio mas de un argumento, solo se permite 0 a 1 argumento");
+            System.exit(1);
+        }
+
         Scanner entradaTerminal = new Scanner(System.in);
         if (args.length == 0) {
             while (true) {
                 System.out.print("REPL-ESCOM: ");
                 if (!entradaTerminal.hasNextLine()) {
                     break;
-                } else if (entradaTerminal.nextLine().equals("salir"))
+                }
+                String entrada = entradaTerminal.nextLine();
+                if (entrada.equalsIgnoreCase("salir")) {
                     break;
+                }
+                System.out.println(entrada);
             }
-        } else if (args.length == 1) {
-            File rutaDelArchivo = new File("/home/salvador/Escritorio/repl.txt");
-            String caracteres = entradaTerminal.nextLine();
+            entradaTerminal.close();
 
+        } else {
+            File rutaDelArchivo = new File("src/repl.txt");
+            if (!rutaDelArchivo.exists() || !rutaDelArchivo.isFile()) {
+                System.out.println("Error: El archivo especificado no existe o no es un archivo válido.");
+                System.exit(1);
+            }
+            System.out.println(rutaDelArchivo);
+            Scanner lecturaArchivo = new Scanner(rutaDelArchivo);
+            while (lecturaArchivo.hasNextLine()) {
+                System.out.println(lecturaArchivo.nextLine());
+            }
         }
+
 
     }
 
