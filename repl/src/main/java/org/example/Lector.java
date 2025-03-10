ecb34b09ea5269a922ffa69681c4e96e4784e8d0
Salvador Sandoval Garibay <emeth.salvador@ceneval.edu.mx>
2025-03-10 06:04:14 -0600

    reapl

---
+++ repl/src/main/java/org/example/Lector.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+
+public class Lector {
+    private String usuario_oracion_guardada;
+    private List<Token> palabrasReservadas;
+    private Integer inicio_oracion;
+    private Integer fin_oracion;
+    private Integer errores_oracion;
+
+}
+
